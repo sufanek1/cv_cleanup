0a017583d572621b5cc0c88f6b07341f49bf4875
root <root@localhost.localdomain>
2019-11-25 13:53:07 +0100

    first version, seems to be working

---
+++ README.md
@@ -0,0 +1,4 @@
+# cv_cleanup
+# Simple script for satellite 6
+# which cleans up unused content-view
+# versions
---
+++ cv_cleanup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+list=$(sudo -u foreman psql -d foreman -t -c 'select katello_content_view_versions.id from katello_content_view_versions left join katello_content_view_environments on (katello_content_view_versions.id = katello_content_view_environments.content_view_version_id) where katello_content_view_environments.id IS NULL;')
+
+for i in $list
+do
+  echo "deleting cv_version id $i"
+  hammer -u admin -p redhat content-view version delete --id $i
+done
